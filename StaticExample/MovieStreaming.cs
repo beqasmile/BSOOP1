371c2bfc0f931f0be714a36f6f9c3f37ef0965e4
Ilya Abramovich <ilya>
2020-04-19 18:10:08 +0300

    first commit

---
+++ StaticExample/MovieStreaming.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstOOPProject
+{
+    public class MovieStreaming
+
+    {
+        private int streamCount;
+        public MovieStreaming()
+        {
+            streamCount = 0;
+        }
+
+        public void StreamMovieNow()
+        {
+            streamCount++;
+        }
+
+    }
+}
--- StaticExample/Program.cs
+++ StaticExample/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FirstOOPProject;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,72 @@ namespace StaticExample
     {
         static void Main(string[] args)
         {
+            List<Movie> myMoviesList = new List<Movie>(); // list creation and definition
+
+            for (int i = 0; i < 5; i++)
+            {
+                Movie myMovie = new Movie(i);
+
+
+                Console.WriteLine("Please input movie name >");
+                String movieName = Console.ReadLine();
+                myMovie.SetMovieName(movieName);
+                Console.WriteLine("Please input movie type >");
+                String movieType = Console.ReadLine();
+                myMovie.SetMovieType(movieType);
+                Console.WriteLine("Please input movie lenght >");
+
+                int movieLength = int.Parse(Console.ReadLine());
+                myMovie.SetMovieLength(movieLength);
+                Console.WriteLine("Please input movie director >");
+
+                String movieDirector = Console.ReadLine();
+                MovieDirector md1 = new MovieDirector(movieDirector);
+                myMovie.SetMovieDirector(md1);
+
+
+                Console.WriteLine("Please input the number of actors>");
+                int numberActors = int.Parse(Console.ReadLine());
+
+
+                for (int j = 0; j < numberActors; j++)
+                {
+                    Console.WriteLine("Please input actor name ");
+                    string actorName = Console.ReadLine();
+
+                    Console.WriteLine("Please input actor sur name ");
+                    string actorSUrName = Console.ReadLine();
+
+
+                    Console.WriteLine("Please input actor type ");
+                    string actorType = Console.ReadLine();
+
+
+                    int birthDay = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Please input bitrhday >");
+
+                    MovieActor movieActor = new MovieActor(i, actorName, actorSUrName, birthDay, actorType);
+
+                    myMovie.AddActor(movieActor);
+
+                }
+
+
+                myMoviesList.Add(myMovie);
+
+
+            }
+
+
+            // myMoviesList.Remove(movie1);                   // remove object from list
+
+            foreach (Movie movieTemp in myMoviesList)       // pass over list
+            {
+                Console.WriteLine(movieTemp.ToString());
+            }
+
+            Console.ReadKey();
+
         }
     }
 }
